2a1a6024723fde7ba37e74b8e4fc57b7c6ab66d4
AleksandrMaslov <amaslov8910@gmail.com>
2024-11-06 23:21:29 +0300

    init commit

---
+++ scripts/setENV.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Exporting ENV variables..."
+
+export DOMAIN="drive.synsys.co"
+echo "Export completed - DOMAIN"
+
+export PROTOCOL="https"
+echo "Export completed - PROTOCOL"
