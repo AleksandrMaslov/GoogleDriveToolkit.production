fbf9c68382277bed8c5796a2f83fb68320aa545e
AleksandrMaslov <amaslov8910@gmail.com>
2024-11-08 19:46:56 +0300

    fix: initpg

--- postgres/initpg.sh
+++ postgres/initpg.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "user: $POSTGRES_USER"
-echo "password: $POSTGRES_PASSWORD"
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE "$POSTGRES_DB";
 EOSQL
