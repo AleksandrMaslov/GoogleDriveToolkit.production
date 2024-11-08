ebb8bc4f9db24d792fc4b943bdae382f8e32efe6
AleksandrMaslov <amaslov8910@gmail.com>
2024-11-08 19:39:13 +0300

    add: initpg echo env

--- postgres/initpg.sh
+++ postgres/initpg.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "user: $POSTGRES_USER"
+echo "password: $POSTGRES_PASSWORD"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE "$POSTGRES_DB";
 EOSQL
