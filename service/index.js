d0933b3ee5c622f29bfeb60a7abd24c7b1bddbe4
AleksandrMaslov <amaslov8910@gmail.com>
2024-11-08 18:28:10 +0300

    add: service endpoints logging

--- service/index.js
+++ service/index.js
@@ -12,24 +12,39 @@ app.options('*', cors());
 
 const run = async () => {
   await client.connect()
+  console.log('DB client connected');
 
   await client.query('CREATE TABLE IF NOT EXISTS mytable (i integer);')
+  console.log('Table created');
+
 
   app.get('/set-increment', async (req, res) => {
-    const last = await client.query('select i from mytable order by i desc limit 1')
-    let toInsert = 0
-    if (last.rows.length === 0) {
-      toInsert = 1
-    } else {
-      toInsert = last.rows[0].i + 1
+    console.log('Set request');
+
+    try {
+      const last = await client.query('select i from mytable order by i desc limit 1')
+      let toInsert = 0
+      if (last.rows.length === 0) {
+        toInsert = 1
+      } else {
+        toInsert = last.rows[0].i + 1
+      }
+      await client.query('insert into mytable (i) values ($1)', [toInsert])
+      res.send('increment updated')
+    } catch (error) {
+      res.send(`${error.message}`)
     }
-    await client.query('insert into mytable (i) values ($1)', [toInsert])
-    res.send('increment updated')
   })
 
   app.get('/get-increment', async (req, res) => {
-    const result = await client.query('select max(i) from mytable')
-    res.send({ max: result.rows[0].max })
+    console.log('Get request');
+
+    try {
+      const result = await client.query('select max(i) from mytable')
+      res.send({ max: result.rows[0].max })
+    } catch (error) {
+      res.send(`${error.message}`)
+    }
   })
 
 
